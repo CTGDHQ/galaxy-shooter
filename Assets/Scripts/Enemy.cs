2deea8150c47934c4f9bad9803bd7faa9daeebc1
Nate Benton <nate@natebenton.net>
2020-05-14 13:46:01 -0400

    Feature: New Enemy Movement
    Added a new enemy movement pattern that's generated when an enemy is spawned. Left and right movement pattern

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,11 +18,21 @@ public class Enemy : MonoBehaviour
 
     private float _canFire = -1f;
 
+    private int _movementType;
+
+    private float _targetLeft, _targetRight;
+
+    private bool _moveLeft = true;
+
     private void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
         _animator = gameObject.GetComponent<Animator>();
         _audioSource = GetComponent<AudioSource>();
+
+        _movementType = Random.Range(0, 2);
+
+        SetMovementTargets();
     }
 
     // Update is called once per frame
@@ -33,6 +43,12 @@ public class Enemy : MonoBehaviour
         FireLasers();
     }
 
+    private void SetMovementTargets()
+    {
+        _targetLeft = (transform.position.x - 2);
+        _targetRight = (transform.position.x + 2);
+    }
+    
     private void Reposition()
     {
         // If reached the bottom theshold, respawn at the top with the random x position
@@ -45,17 +61,48 @@ public class Enemy : MonoBehaviour
         var randomX = Random.Range(-9.37f, 9.37f);
 
         transform.position = new Vector2(randomX, 7f);
+
+        SetMovementTargets();
     }
 
     private void Movement()
     {
-        //move down
-        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        switch (_movementType)
+        {
+            case 0:
+                //move down
+                transform.Translate(Vector3.down * _speed * Time.deltaTime);
+                break;
+            case 1:
+                if (_moveLeft)
+                {
+                    if (transform.position.x > _targetLeft)
+                    {
+                        transform.Translate(new Vector3(-0.5f * _speed * Time.deltaTime, -1f * _speed * Time.deltaTime, 0f));
+                    }
+                    else
+                    {
+                        _moveLeft = false;
+                    }
+                }
+                else
+                {
+                    if (transform.position.x < _targetRight)
+                    {
+                        transform.Translate(new Vector3(0.5f * _speed * Time.deltaTime, -1f * _speed * Time.deltaTime, 0f));
+                    }
+                    else
+                    {
+                        _moveLeft = true;
+                    }
+                }
+                break;
+        }
     }
 
     private void FireLasers()
     {
-        if (Time.time > _canFire)
+        if (Time.time > _canFire && _speed != 0)
         {
             _fireRate = Random.Range(3.0f, 7.0f);
             _canFire = (Time.time + _fireRate);
@@ -84,7 +131,6 @@ public class Enemy : MonoBehaviour
             _player.AddToScore(10);
 
             _animator.SetTrigger("OnEnemyDeath");
-            _fireRate = 0f;
             _speed = 0;
             _audioSource.Play();
             Destroy(GetComponent<Collider2D>());
